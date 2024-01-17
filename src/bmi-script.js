bdd193be1888f2b73b66dfa7115bdf0395982b4d
Naijaoracle <134971234+Naijaoracle@users.noreply.github.com>
2024-01-17 20:39:54 +0000

    pointer calibration

--- src/bmi-script.js
+++ src/bmi-script.js
@@ -49,7 +49,7 @@ function updateCategoryBar(bmi) {
   if (categoryIndex !== -1) {
     const categoryBarWidth = categoryBar.offsetWidth; // Get the width of the category bar
     const categoryWidth = categoryBarWidth / categories.length; // Get the width of each category
-    const pointerPosition = categoryIndex * categoryWidth + ((bmi - categories[categoryIndex].min) / (categories[categoryIndex].max - categories[categoryIndex].min)) * categoryWidth;
+    const pointerPosition = categoryIndex * categoryWidth + ((bmi - categories[categoryIndex].min) / (categories[categoryIndex].max - categories[categoryIndex].min + 1)) * categoryWidth;
     pointer.style.left = `${pointerPosition}px`; // Set the left position of the pointer
 
     const categoryElements = categoryBar.children;
