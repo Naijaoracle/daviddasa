5865df9a7df24d58ded71ec78eaa40a98d9aaf21
Naijaoracle <134971234+Naijaoracle@users.noreply.github.com>
2024-01-28 19:07:41 +0000

    centre on chart x-axis, front end fix

--- src/hearingScreening.js
+++ src/hearingScreening.js
@@ -64,13 +64,15 @@ function answer(response) {
   const frequency = parseFloat(frequencyInput.value);
 
   chartData.labels.push(frequency.toString());
+  const middleX = 10010; // Middle of the x-axis range
+  
   if (response === 'Yes') {
-    chartData.datasets[0].data.push({ x: 0, y: frequency });
-    chartData.datasets[1].data.push({ x: frequency, y: 0 }); // Change here
+    chartData.datasets[0].data.push({ x: middleX, y: frequency });
+    chartData.datasets[1].data.push({ x: frequency, y: middleX });
   } else {
-    chartData.datasets[0].data.push({ x: frequency, y: 0 }); // Change here
-    chartData.datasets[1].data.push({ x: 0, y: frequency });
-  }
+    chartData.datasets[0].data.push({ x: frequency, y: middleX });
+    chartData.datasets[1].data.push({ x: middleX, y: frequency });
+  }  
 
   if (chart) {
     chart.update(); // Update the chart immediately after the user responds
@@ -82,7 +84,7 @@ function answer(response) {
         scales: {
           x: {
             type: 'linear', // Change the x-axis type to linear
-            position: 'center', // Center the x-axis
+            position: 'bottom', 
             title: {
               display: false,
             },
